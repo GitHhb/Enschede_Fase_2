bb50c1446810c4019f51e91a9e2df8c97eb30fe7
GitHhb <github@mittere.net>
2016-05-23 01:32:33 +0200

    Use enum for CarType

--- eu/programit/autofabriek/Main.java
+++ eu/programit/autofabriek/Main.java
@@ -6,23 +6,25 @@ abstract class Car {
     public abstract void drive();
 }
 
+enum CarType {BMW, AlfaRomeo, Lambo}
+
 public class Main {
     public static void main(String[] args) {
-        Car c = CarFactory.getInstance("BMW");
+        Car c = CarFactory.getInstance(CarType.BMW);
         c.drive();
-        CarFactory.getInstance("AlfaRomeo").drive();
+        CarFactory.getInstance(CarType.AlfaRomeo).drive();
     }
 }
 
 final class CarFactory {
-    public static Car getInstance(String type) {
+    public static Car getInstance(CarType type) {
 //        Car c = new Constructor<type>.newInstance();
         switch (type) {
-            case "BMW":
+            case BMW:
                 return new BMW();
-            case "AlfaRomeo":
+            case AlfaRomeo:
                 return new AlfaRomeo();
-            case "Lambo":
+            case Lambo:
                 return new Lambo();
         }
         return null;
